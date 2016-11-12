535d548039a1027cd141954231e284cb3ec9106f
Steven Castroverde <stevencastroverde@Stevens-MacBook-Air.local>
2016-11-12 09:13:41 -0700

    allows user to select show

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,44 +1,67 @@
 var searchUrl = 'http://api-public.guidebox.com/v1.43/US/rKcE8UjpWG7r8hIkG3Dus9HltJxmoYxp/search/title/';
 var tvIdUrl = 'http://api-public.guidebox.com/v1.43/US/rKcE8UjpWG7r8hIkG3Dus9HltJxmoYxp/show/';
+var showParams = '/episodes/all/1/100/';
+var webContent = '/web/true';
 
-// $.get(baseUrl , searchContent)
-$('#submitbutton').on('click', function() {
+
+// clear search results when clear button is hit
+$('#resetbutton').on('click', function (){
+  $('.results').empty();
+
+});
+
+
+
+// When form is submitted get values
+$('#submitbutton').on('click', function () {
     event.preventDefault();
     var title = $('#searchbar').val();
 		console.log(title);
 
-		$.get(searchUrl +title , function (data){
+
+
+// Display all shows with title matches
+		$.get(searchUrl + title , function (data){
 			console.log(data);
 			for (var i = 0; i < data.results.length; ++i){
-				$('.results').append('<div>'
+				$('.results').append('<section' + ' id ="' + data.results[i].id +'">'
 					+  '<h3>' + data.results[i].title + ' ' +  data.results[i].id + '</h3>'
 					+	'<img src="' + data.results[i].artwork_304x171 + '"/>'
 						+ '<hr />'
-							+ '</div>');
-
-	$('.results div').on('click', function(){
-		var selected = $('this').event.data;
-		console.log(selected);
-
-	})
-
-
-}
+							+ '</section>');
+	}
+})
+})
 
 
 
-		})
-
-
-
-
-
-
-
-
+$('.results').on('click','section', function (event){
+  $('.results').empty();
+  var selected = $(this).attr('id');
+  $.get(tvIdUrl + selected , function(data){
+    $('.results').append('<div>'
+      + '<img src="' + data.banner + '"/>'
+      + '<h1>' + data.title + '</h2>'
+      + '<p>' + data.overview + '</p>'
++ '</div>'  );
+return tvIdUrl + selected + showParams + 'hulu_plus' + webContent;
+$.then(function (){
+     console.log( data.results[0].title );
+})
 
 
 
 
 
-	})
+
+
+
+
+
+
+
+
+  })
+
+
+})
