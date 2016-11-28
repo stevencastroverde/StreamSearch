776c95a63279d13a454267a5870295912a88c4c4
Steven Castroverde <stevencastroverde@Stevens-MacBook-Air.local>
2016-11-28 16:38:25 -0700

    got constructor to work

--- assets/js/moviesearch.js
+++ assets/js/moviesearch.js
@@ -1,8 +1,44 @@
 var searchUrl = 'https://api-public.guidebox.com/v1.43/US/rKcE8UjpWG7r8hIkG3Dus9HltJxmoYxp/search/movie/title/';
 var movieIdUrl = 'https://api-public.guidebox.com/v1.43/US/rKcE8UjpWG7r8hIkG3Dus9HltJxmoYxp/movie/';
-var checkValues = null;
+var checkValues = [];
 var resultsDiv = $('.results');
+var firstcall = new API(searchUrl);
 
+
+function API(url) {
+    this.data = null;
+    this.askForData = function(title, callback) {
+        $.get(url + title, function(info) {
+            data = info;
+            console.log(info)
+            callback(info);
+
+        })
+    }
+};
+
+function printMovies(info) {
+    for (var i = 0; i < info.results.length; i++) {
+      var movie = info.results[i];
+        resultsDiv.append('<li class="col l4 m6 s12 row hoverable"' + 'id ="' + movie.id + '">' +
+            '<div class="card">' +
+            '<span class="card-title truncate">' + movie.title + '</span>' +
+            '<div class="card-image"> <img src="' + movie.poster_240x342 + '"/>' +
+            '</div></div>' +
+            '</li>');
+    }
+
+
+
+}
+var checkedboxes = function(checkValues) {
+    $.each($('input[type=checkbox]:checked'), function() {
+        checkValues.push($(this).val());
+        return checkValues;
+
+    });
+
+};
 
 // clear search results when clear button is hit
 $('#resetbutton').on('click', function() {
@@ -17,53 +53,31 @@ $('#submitbutton').on('click', function() {
     event.preventDefault();
     resultsDiv.empty();
     var title = $('#searchbar').val();
-
-    var checkValues = [];
-
-		$.each($('input[type=checkbox]:checked'), function() {
-				checkValues.push($(this).val());
-        return checkValues;
-		});
-
-    // Display all shows with title matches
-    $.get(searchUrl + title, function(data) {
-        for (var i = 0; i < data.results.length; ++i) {
-            resultsDiv.append('<li class="col l4 m6 s12 row hoverable"' + 'id ="' + data.results[i].id + '">' +
-                '<div class="card">' +
-                // '<span class="card-title">' + data.results[i].title + '</span>' +
-                '<div class="card-image"> <img src="' + data.results[i].poster_240x342 + '"/>' +
-                '</div></div>' +
-                '</li>');
-        }
-    })
-})
+    checkedboxes(checkValues);
+    firstcall.askForData(title, printMovies);
+});
 
 
 
 resultsDiv.on('click', 'li', function() {
     resultsDiv.empty();
     var selected = $(this).attr('id');
-    $.get(movieIdUrl + selected, function(data,checkValues) {
-            resultsDiv.append('<div class="row movie_poster">' +
-                '<img class="col s12 m6" src="' + data.poster_400x570 + '"/>' +
-                '<h3 class=" grey-text text-darken-4 col s12 m6 ">' + data.title + '</h3>'
-                 + '<h4 class=" col s12 m6 grey-text text-darken-1  ">' + data.release_year + '</h4>'
-              +  '<p class="col s12 m6 grey-text text-darken-1  ">' + data.overview + '</p>'
-               + '<p id="link"></p>'
-							+ '</div>');
+    $.get(movieIdUrl + selected, function(data, checkValues) {
+        resultsDiv.append('<div class="row movie_poster">' +
+            '<img class="col s12 m6" src="' + data.poster_400x570 + '"/>' +
+            '<h3 class=" grey-text text-darken-4 col s12 m6 ">' + data.title + '</h3>' +
+            '<h4 class=" col s12 m6 grey-text text-darken-1  ">' + data.release_year + '</h4>' +
+            '<p class="col s12 m6 grey-text text-darken-1  ">' + data.overview + '</p>' +
+            '<p id="link"></p>' +
+            '</div>');
 
-										if (data.subscription_web_sources.length > 0){
-										$('#link').append('<a class="white-text  lime btn center-align" href="' + data.subscription_web_sources[0].link + '">Watch Now</a>')
+        if (data.subscription_web_sources.length > 0) {
+            $('#link').append('<a class="white-text  lime btn center-align" href="' + data.subscription_web_sources[0].link + '">Watch Now</a>')
 
-							 		} else {
-							 			$('#link').append('<h4 class=" grey-text text-darken-4 col s12 m6 "> Sorry the subscriptions you use do not have this movie</h4>');
-
-
-                	}
-							 	})
-							 }
+        } else {
+            $('#link').append('<h4 class=" grey-text text-darken-4 col s12 m6 "> Sorry the subscriptions you use do not have this movie</h4>');
 
 
-
-
-        )
+        }
+    })
+})
